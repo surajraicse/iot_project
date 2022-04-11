48c24aecea6fa705acb9b24969e7d464c3f21342
surajraicse <surajrai.cse@gmail.com>
2022-04-11 14:16:04 +0530

    'edited admin.py of owner'

--- iotsite/admin/models.py
+++ iotsite/admin/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+  USER_TYPE_CHOICES = (
+      (1, 'admin'),
+      (2, 'police'),
+      (3, 'owner'),
+      (4, 'vendor'),
+  )
+  user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
