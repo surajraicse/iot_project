ab94079c5f8424426e68371566d7e092cd51b46c
surajraicse <surajrai.cse@gmail.com>
2022-04-11 11:50:23 +0530

    'Settings.py and urls.py updated for JWT'

--- iotsite/iotsite/settings.py
+++ iotsite/iotsite/settings.py
@@ -71,6 +71,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'iotsite.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
--- iotsite/iotsite/urls.py
+++ iotsite/iotsite/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 
 urlpatterns = [
     path('',include('owner.urls')),
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 	
