9fa4f155a7d82b75fad6edc754800d10c74c48dd
surajraicse <surajrai.cse@gmail.com>
2022-04-11 17:18:39 +0530

    'created model account'

--- iotsite/accounts/models.py
+++ iotsite/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    id = models.CharField(max_length = 50 ,primary_key=True, unique=True)
+    password = models.CharField(max_length = 20)
+    USER_TYPE_CHOICES = (
+      (1, 'admin'),
+      (2, 'police'),
+      (3, 'owner'),
+      (4, 'vendor'),
+    )
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+    is_active = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.id
+
